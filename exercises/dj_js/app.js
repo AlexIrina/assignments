a29c872deddc74f3f69272975c2be06b79324e84
Alex George <alexgeorge2863@gmail.com>
2020-01-10 15:14:57 -0700

    added js

---
+++ exercises/dj_js/app.js
@@ -0,0 +1,47 @@
+// Blue when the mouse hovers over the square
+
+// Red when the mouse button is held down over the square
+// Yellow when the mouse button is let go over the square
+// Green when the mouse is double clicked in the square
+// Orange when the mouse scroll is used somewhere in the window (not just over the square).
+
+// let header = document.getElementById("header");
+
+// // Blue when the mouse hovers over the square
+// header.addEventListener("mouseover", function() {
+//   // highlight the mouseenter target
+//   header.style.backgroundColor = "blue";
+// });
+
+// // Red when the mouse button is held down over the square
+// header.addEventListener("mouseup", function() {
+//   // highlight the mouseenter target
+//   header.style.backgroundColor = "red";
+// });
+
+// // Yellow when the mouse button is let go over the square
+// header.addEventListener("mousedown", function() {
+//   // highlight the mouseenter target
+//   header.style.backgroundColor = "yellow";
+// });
+
+// // Green when the mouse is double clicked in the square
+// header.addEventListener("dblclick", function() {
+//   // highlight the mouseenter target
+//   header.style.backgroundColor = "green";
+// });
+
+// // Orange when the mouse scroll is used somewhere in the window (not just over the square).
+
+// header.addEventListener("wheel", function() {
+//   // highlight the mouseenter target
+//   header.style.backgroundColor = "orange";
+// });
+
+// document.getElementById("body").addEventListener("wheel", function() {
+//   // highlight the mouseenter target
+//   document.body.style.backgroundColor = "orange";
+// });
+
+// You should also be able to press the first letter of the colors (typing "r" on the keyboard for "red", for example) and have the box change to that color. Check
+
