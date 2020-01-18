0417c4f64cba97b48f26ca6a863b3e5ad763158a
Alex George <alexgeorge2863@gmail.com>
2020-01-17 17:08:05 -0700

    completed the game

--- exercises/module_1/escape_room/app.js
+++ exercises/module_1/escape_room/app.js
@@ -1,11 +1,57 @@
-let readline = require('readline-sync');
-let name = readline.question(`What is your name? `);
+let readline = require(`readline-sync`);
 
-const message = `\n WELCOME ${name}!! \n \n Please choose one of the following:`;
+// ask the name
+let name = readline.question(
+  '\n \n                                                  <-------- What is your name? ---------> \n'
+);
+console.log(
+  `\n\n                                          <---------  Hello ${name}! Welcome to the Escape Room! ----------> \n`
+);
 
-let options = [`Put your hand in the hole`, `Find the key`, `Open the door`];
-console.log(message);
+let isDead = false;
+let hasKey = false;
 
-let index = readline.keyInSelect(options);
+// User options
+let options = [
+  `\n Put hand in hole \n---------------------|`,
+  `\n Find the key \n------------------|`,
+  `\n Open door \n--------------|`
+];
 
-console.log(`Okay ${name}, you chose to:`, options[index]);
+// Game loop continues until the game ends
+while (!isDead) {
+  let userChoice = readline.keyInSelect(
+    options,
+    ` \n                                <---------- Choose one of the following: ---------->\n \n \n`
+  );
+  if (userChoice === 0) {
+    console.log(
+      `  \n\n                 <------------  ${name}, your hand is stuck. You will be dead soon.  ------------>\n`
+    );
+    isDead = true;
+    break;
+  } else if (userChoice === 1) {
+    console.log(
+      `\n \n \n                              <---------- You found the key! Now you need to find the door  -----------> \n`
+    );
+    hasKey = true;
+  } else if (userChoice === 2 && !hasKey) {
+    console.log(
+      `     \n                  <--------------- You found the door. It's locked! You don't have the key to open it  ----------> \n`
+    );
+  } else if (userChoice === 2 && hasKey) {
+    console.log(
+      `\n                          <---------- ${name} you've managed to escape. CONGRATULATIONS!! --------------> \n`
+    );
+    break;
+  } else if (userChoice === -1) {
+    console.log(
+      `\n                                <------------ You are trapped in the Escape Room. Cancelling is not an option.  ---------> \n`
+    );
+  }
+}
+if (isDead) {
+  console.log(
+    `\n                             <--------- You're dead. GAME OVER!! Please Try again. --------->  \n`
+  );
+}
