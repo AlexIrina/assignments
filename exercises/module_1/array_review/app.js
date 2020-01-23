4a2fb75ff5b4202309c37c4ebfe31b6b78d1690b
Alex George <alexgeorge2863@gmail.com>
2020-01-23 12:33:56 -0700

    completed looping array review

--- exercises/module_1/array_review/app.js
+++ exercises/module_1/array_review/app.js
@@ -3,7 +3,7 @@
 // // array of strings
 // let turtles = ['Raphael', 'Donatello', 'Michelangelo', 'Leonardo'];
 // // array of objects
-// var users = [
+// let users = [
 //   {
 //     name: 'Joe Schmoe',
 //     age: 42,
@@ -114,7 +114,7 @@
 Loops allow programmers run the same sets of code many time until the condition is met
  * 
  * 2. In a for loop, what are the 3 main parts that go into the parentheses?
- * 1. initialize variables, 2. condition - should I run one more time?, 3. final code that we want to run at the end of the loop
+ * 1. initialize iterables, 2. condition - should I run one more time?, 3. final code that we want to run at the end of the loop
  * 
  * 3. Write a loop that logs the numbers 15 to 50 (inclusive) to the console.
  * for (let i = 15; i <= 50; i++) {
@@ -134,3 +134,62 @@ Loops allow programmers run the same sets of code many time until the condition 
 // for (let i = 0; i < 10; i++) {
 //   i === 7 ? console.log('its your lucky day') : console.log(i);
 // }
+
+////////////////////////////// Review - Arrays and Loops
+
+// let numbersArr = [23, 64, 20, 33, 40, 100, 30, 50, 2];
+// let petsArr = ['cat', 'dog', 'bird', 'snake', 'rabbit'];
+// let users = [
+//   {
+//     name: 'joe',
+//     age: 30
+//   },
+//   {
+//     name: 'steve',
+//     age: 23
+//   },
+//   {
+//     name: 'sarah',
+//     age: 39
+//   },
+//   {
+//     name: 'lisa',
+//     age: 27
+//   }
+// ];
+
+/* QUIZ
+    1. What part of the for loop will allow you to access each individual item in an array?
+    the iterator or variable[i]
+    
+    2. What part of the for loop will need the array's length in order to run the correct amount of iterations?
+    variable.length
+
+    3. Using the pets array, how would you write a for loop to console.log all of the pets with an "s" added to the end? Your console log's should be ( cats, dogs, birds, snakes, rabbits )
+    for (let i = 0; i < petsArr.length; i++) {
+     console.log(`${petsArr[i]}s`);
+    }
+    
+    4. Using the numbers array, how would you write a for loop to console log only the odd numbers?
+
+    // let numbersArr = [23, 64, 20, 33, 40, 100, 30, 50, 2];
+    //  for (let i = 0; i < numbersArr.length; i++) {
+     //   if (numbersArr[i] % 3 === 0) console.log(numbersArr[i]);
+    //  }
+    
+    // 5. Using the users array, how would you write a for loop that would console log the following sentence for each user in the array. ("Hi, I am joe and I am 30 years old")
+
+    // for (let i = 0; i < users.length; i++) {
+    //   console.log(`Hi, I am ${users[i].name} and I am ${users[i].age} years old`);
+    // }
+
+  
+  // 6. Using the pets array, how would you write a for loop that would console.log all the pets in
+// //     reverse order?
+
+// let petsArr = ['cat', 'dog', 'bird', 'snake', 'rabbit'];
+
+// for (let i = petsArr.length - 1; i >= 0; i--) {
+//   console.log(petsArr[i]);
+// }
+*/
