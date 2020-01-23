79edc36b34054ce2f1be66e809c0a0c0faf6f284
Alex George <alexgeorge2863@gmail.com>
2020-01-23 13:44:17 -0700

    completed nested for loop review

---
+++ exercises/module_1/nested_for_loops_101/app.js
@@ -0,0 +1,47 @@
+// Nested Loops
+
+// let arr = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6]
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr[i].length; j++) {
+//     console.log(arr[i][j]);
+//   }
+// }
+
+// Nested Loops
+
+let arrayOfArrays = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+// for (let i = 0; i < arrayOfArrays.length; i++) {
+//   console.log(arrayOfArrays[i]); //this returns the 3 arrays inside the array
+// }
+
+// to access each individual item inside the array i need to have another for loop inside that for loop
+
+// for (let i = 0; i < arrayOfArrays.length; i++) {
+//   // console.log(arrayOfArrays[i]); //this returns the 3 arrays inside the array
+//   for (let j = 0; j < arrayOfArrays[i].length; j++) {
+//     console.log(arrayOfArrays[i][j]); //this returns all the numbers inside the array
+//   }
+// }
+
+// [1, 2, 3],
+// [4, 5, 6],
+// [7, 8, 9]
+
+// 1. console.log the number 6
+// console.log( arrayOfArrays[1][2] )
+
+// 2. console.log the number 1
+console.log(arrayOfArrays[0][0]);
+
+// 3. console.log the number 9
+console.log(arrayOfArrays[2][2]);
