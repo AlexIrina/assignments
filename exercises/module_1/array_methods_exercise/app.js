3fa9502ae151483b18e5dd73705ba718ba4ed33f
Alex George <alexgeorge2863@gmail.com>
2020-01-23 18:11:03 -0700

    completed the exercise

---
+++ exercises/module_1/array_methods_exercise/app.js
@@ -0,0 +1,30 @@
+let fruit = ['banana', 'apple', 'orange', 'watermelon'];
+let vegetables = ['carrot', 'tomato', 'pepper', 'lettuce'];
+
+// // Remove the last item from the vegetable array.
+// console.log(`OUTPUT: vegetables`, vegetables.pop());
+// // Remove the first item from the fruit array.
+// console.log(fruit.shift());
+// // Find the index of "orange."
+// console.log(fruit.indexOf('orange'));
+// Add that number to the end of the fruit array.
+// console.log(fruit.push(2));
+// console.log(fruit);
+// // // Use the length property to find the length of the vegetable array.
+// console.log(vegetables.length);
+// // Add that number to the end of the vegetable array.
+// vegetables.push(vegetables.length);
+// console.log(vegetables);
+// // // Put the two arrays together into one array. Fruit first. Call the new Array "food".
+// let food = fruit.concat(vegetables);
+// // console.log(`OUTPUT: food`, food);
+// // // Remove 2 elements from your new array starting at index 4 with one method.
+// // console.log(food.splice(4, 2));
+// // Reverse your array.
+// let reversedArr = food.reverse();
+// // Turn the array into a string and return it.
+// // let stringedArr = String(reversedArr);
+// console.log(reversedArr.join(''));
+// console.log(reversedArr.toString(''));
+
+// If you've done everything correctly, the last step should print the following string to the console:
