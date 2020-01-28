300eb0f0d99f9e48b803a3f80a18e7012e34b2cd
Alex George <alexgeorge2863@gmail.com>
2020-01-28 12:01:04 -0700

    started this assignment

---
+++ exercises/module_1/bugs-on-a-plane/app.js
@@ -0,0 +1,27 @@
+const form = document.getElementByID("arline-form");
+const submit = document.getElementByID(submit);
+const query = document.querySelector;
+
+function formAlert() {
+    let firstName = form.elements["firstName"].value;
+    let lastName = form.elements["lastName"].value;
+    let age = form.elements["age"].value;
+    let gender = form.elements["gender"].value;
+    let location = form.elements["travel-location"].value;
+    let diet = [];
+    if (form.elements['vegan'].checked) {
+        let diet.pop(document.getElementById("vegan").value);
+    }
+    if (form.elements['gluten'].checked) {
+        diet.push(document.getElementById('gluten').value);
+    }
+    if (form.elements['paleo'].checked) {
+        diet.push(document.getElementById('paleo').value);
+    }
+
+
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+}
+
+
+submit.addEventListener("click", formAlert);
