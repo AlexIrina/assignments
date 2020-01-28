d37be5fbce3e16e7ae54c420868f20d8e4666190
Alex George <alexgeorge2863@gmail.com>
2020-01-28 12:55:02 -0700

    completed the assignment

--- exercises/module_1/bugs-on-a-plane/app.js
+++ exercises/module_1/bugs-on-a-plane/app.js
@@ -1,27 +1,38 @@
-const form = document.getElementByID("arline-form");
-const submit = document.getElementByID(submit);
-const query = document.querySelector;
+const form = document.getElementById('airline-form');
+const submit = document.getElementById(`submit`);
 
 function formAlert() {
-    let firstName = form.elements["firstName"].value;
-    let lastName = form.elements["lastName"].value;
-    let age = form.elements["age"].value;
-    let gender = form.elements["gender"].value;
-    let location = form.elements["travel-location"].value;
-    let diet = [];
-    if (form.elements['vegan'].checked) {
-        let diet.pop(document.getElementById("vegan").value);
-    }
-    if (form.elements['gluten'].checked) {
-        diet.push(document.getElementById('gluten').value);
-    }
-    if (form.elements['paleo'].checked) {
-        diet.push(document.getElementById('paleo').value);
-    }
+  let firstName = form.elements['first-name'].value;
+  console.log(firstName);
+  let lastName = form.elements['last-name'].value;
+  console.log(lastName);
 
+  let age = form.elements['age'].value;
 
-    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+  let gender = form.elements['gender'].value;
+
+  let location = form.elements['travel-location'].value;
+
+  let diet = [];
+
+  if (form.elements['vegan'].checked) {
+    diet.push(document.getElementById('vegan').value);
+  } else if (form.elements['gluten'].checked) {
+    diet.push(document.getElementById('gluten').value);
+  } else if (form.elements['paleo'].checked) {
+    diet.push(document.getElementById('paleo').value);
+  }
+
+  alert(
+    `First Name: ${firstName} 
+      \n Last Name: ${lastName} 
+      \nAge: ${age} 
+      \nGender: ${gender} 
+      \nTravel Location: ${location} 
+      \nDiet: ${diet} 
+      \nAwesome, now if you die, it won't be an accident..`
+  );
 }
 
-
-submit.addEventListener("click", formAlert);
+// A pop up box should appear when you click the submit button as shown in the image above.
+submit.addEventListener('click', formAlert);
