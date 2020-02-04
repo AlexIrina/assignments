0e485db81198b4710adfc9c2b1cf2a2ed8d09421
Alex George <alexgeorge2863@gmail.com>
2020-02-03 17:52:59 -0700

    added notes

---
+++ exercises/module_2/forms_2/app.js
@@ -0,0 +1,31 @@
+const form = document.myForm;
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // Circle Radio Inputs
+  // // return genders value
+  // console.log(form.gender.value);
+
+  // Checked Box Inputs
+  //1 returns an array of the Games Movies checkboxes. I need to find out which ones are checked
+  // console.log(form.entertainment);
+
+  //2 for loop
+  for (let i = 0; i < form.entertainment.length; i++) {
+    // 5 push the checked value into this array
+    const checkedInputs = [];
+
+    // lets me loop over each item thats checked
+    // 3 .checked
+    // console.log(form.entertainment[i].checked);
+    // check boxes in the html
+
+    // 4 Checks if current iteration is checked.
+    if (form.entertainment[i].checked) {
+      //if true
+      //If its is checked then get its value
+      checkedInputs.push(form.entertainment[i].value);
+    }
+  }
+  console.log(checkedInputs);
+});
