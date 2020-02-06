5fffaba282183ec483144a012b007013e1d8be7d
Alex George <alexgeorge2863@gmail.com>
2020-02-06 10:28:29 -0700

    small changes in js

--- exercises/module_2/marios_pest_control/app.js
+++ exercises/module_2/marios_pest_control/app.js
@@ -9,10 +9,10 @@ const cheepcheepsInput = form.cheepcheepsInput;
 // grabs the result output
 const outputResult = form.result;
 
-// grab each character's div so that it can be clicked
-const bobomb = document['bob-omb'];
-const goomba = document['goomba'];
-const cheepcheep = document['cheep-cheep'];
+// // grab each character's div so that it can be clicked
+// const bobomb = document.bobomb;
+// const goomba = document.goomba;
+// const cheepcheep = document.cheepcheep;
 
 // Create a submit event listener for the whole form
 form.addEventListener('submit', addItAllUp);
@@ -23,7 +23,6 @@ function addItAllUp(event) {
   event.preventDefault();
 
   // first initialize all fields that haven't been touched
-  // rather than requiring them (ease of use)
   if (bobombsInput.value === '') {
     bobombsInput.value = 0;
   }
@@ -46,10 +45,15 @@ function addItAllUp(event) {
   // update the output to display the total
   outputResult.value = result;
 
-  // reset all the fields
+  // reset all the fields after submit is clicked
   bobombsInput.value = '';
   goombasInput.value = '';
   cheepcheepsInput.value = '';
+
+  // document.body.style.backgroundImage = "url('images/bgimage.jpg')";
+
+  // // resets background image
+  // document.body.style.backgroundColor = 'bisque';
 }
 
 // When a character is clicked, add 1 to its field
