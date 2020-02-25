32d8bb202dea80b5fe5aabf99d160820cb615b2a
Alex George <alexgeorge2863@gmail.com>
2020-02-25 07:14:54 -0700

    added complete and delete to the ui

--- exercises/module_2/axios_todo_project/todo.js
+++ exercises/module_2/axios_todo_project/todo.js
@@ -1,11 +1,10 @@
 const title = document.getElementById('title');
 const notes = document.getElementById('notes');
 const price = document.getElementById('price');
-// const imageUrl = document.getElementById('imageUrl');
+const imageUrl = document.getElementById('imageUrl');
 const submit = document.getElementById('submit');
 const todoForm = document['todo-form'];
 
-// GET DATA
 // GET's THE TODO's FROM THE DATABASE
 function getData() {
   axios
@@ -20,11 +19,13 @@ function listData(data) {
   clearList();
 
   for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
     const list = document.getElementById('todo-list');
     const row = document.createElement('tr');
     const title = document.createElement('td');
     const description = document.createElement('td');
     const price = document.createElement('td');
+    const imageUrl = document.createElement('td');
     const checkBox = document.createElement('td');
     const deleted = document.createElement('td');
     const update = document.createElement('td');
@@ -32,12 +33,18 @@ function listData(data) {
     title.textContent = data[i].title;
     description.textContent = data[i].description;
     price.textContent = data[i].price;
+    imageUrl.textContent = data[i].imageUrl;
     checkBox.textContent = data[i].checkBox;
     deleted.textContent = data[i].deleted;
     update.textContent = data[i].update;
 
+    if (data[i].completed) {
+      description.className = 'completed';
+    }
+
     const checkBtn = document.createElement('p');
     checkBtn.textContent = '√';
+    checkBtn.style.color = 'blue';
     checkBtn.addEventListener('click', () => {
       isCompleted(data[i]);
     });
@@ -45,6 +52,7 @@ function listData(data) {
     // creating delete button when clicked should delete the item
     const deleteBtn = document.createElement('p');
     deleteBtn.textContent = 'X';
+    deleteBtn.style.color = 'red';
     deleteBtn.addEventListener('click', () => {
       deleteItem(data[i]);
     });
@@ -52,6 +60,7 @@ function listData(data) {
     row.append(title);
     row.append(description);
     row.append(price);
+    row.append(imageUrl);
     row.append(checkBox);
     row.append(deleted);
     row.append(update);
@@ -64,14 +73,18 @@ function listData(data) {
     document.getElementById('todo-list').appendChild(row);
   }
 }
-// if item is completed
+
+// If item is completed
 function isCompleted(todo) {
+  console.log('todo', todo);
   axios
     .put(`https://api.vschool.io/alex2/todo/${todo._id}`, {
       ...todo,
       completed: !todo.completed
     })
+
     .then(res => console.log(res.data))
+    .then(res => getData())
     .catch(err => console.log(err));
 }
 
@@ -102,10 +115,11 @@ todoForm.addEventListener('submit', function(e) {
   const newTodo = {
     title: todoForm.title.value,
     description: todoForm.notes.value,
-    price: todoForm.price.value
+    price: todoForm.price.value,
+    imageUrl: todoForm.imageUrl.value
   };
   // to do form disappears
-  todoForm.title.value = '';
+  clearFields();
 
   axios
     .post('https://api.vschool.io/alex2/todo', newTodo)
@@ -116,12 +130,19 @@ todoForm.addEventListener('submit', function(e) {
 // DELETE requests
 // DELETE an existing todo: https://api.vschool.io/<yourname>/todo/<todoId>
 
-function deleteItem() {
-  const `deleteButton` = document.getElementById('delete-button');
-  `deleteButton`.addEventListener('click', function() {
-    axios
-      .delete(`https://api.vschool.io/alex2/todo/${todo._id}`)
-      .then(response => console.log(response.data))
-      .catch(error => console.log(error));
-  });
+function deleteItem(todo) {
+  // const deleteButton = document.getElementById('delete-button');
+  axios
+    .delete(`https://api.vschool.io/alex2/todo/${todo._id}`)
+    .then(response => console.log(response.data))
+    .then(getData)
+    .catch(error => console.log(error));
+}
+
+// clear fields
+function clearFields() {
+  document.getElementById('title').value = '';
+  document.getElementById('notes').value = '';
+  document.getElementById('price').value = '';
+  document.getElementById('imageUrl').value = '';
 }
