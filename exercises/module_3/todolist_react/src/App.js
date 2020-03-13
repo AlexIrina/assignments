b00eaa248b1bb0a863ecd9aa033b083795bac345
Alex George <alexgeorge2863@gmail.com>
2020-03-13 15:13:11 -0600

    created todolist with react

---
+++ exercises/module_3/todolist_react/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import todosData from './todosData';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item}/>
+    ));
+
+    return <div className='todo-list'>{todoItems}</div>;
+  }
+}
+
+export default App;
