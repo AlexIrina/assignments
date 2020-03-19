3b7792b00511cfb3f944202865ffbff7bdce7a56
Alex George <alexgeorge2863@gmail.com>
2020-03-19 17:07:28 -0600

    minor changes

--- exercises/module_3/react_dj/src/App.js
+++ exercises/module_3/react_dj/src/App.js
@@ -8,29 +8,49 @@ class App extends React.Component {
     this.state = {
       colors: ['white', 'white', 'white', 'white']
     };
-    this.changeColor = this.changeColor.bind(this);
+    this.smallTimeDj = this.smallTimeDj.bind(this);
   }
 
-  changeColor = () => {
+  smallTimeDj() {
+    console.log('small time dj');
     this.setState(prevState => {
       return {
-        colors: prevState.colors === 'white' ? 'black' : 'white'
+        colors: prevState.colors.map(color => {
+          if (color === 'white') {
+            return 'black';
+          } else if (color === 'black') {
+            return 'white';
+          } else {
+            return color;
+          }
+        })
       };
     });
-  };
+  }
 
   render() {
     return (
       <div id='container'>
         <h1>DJ React </h1>
+
         <div>
           <Square color={this.state.colors[0]} />
+        </div>
+
+        <div>
           <Square color={this.state.colors[1]} />
+        </div>
+
+        <div>
           <Square color={this.state.colors[2]} />
+        </div>
+
+        <div>
           <Square color={this.state.colors[3]} />
         </div>
+
         <div id='btn-container'>
-          <button>Click me</button>
+          <button onClick={this.smallTimeDj}>Click me</button>
         </div>
       </div>
     );
