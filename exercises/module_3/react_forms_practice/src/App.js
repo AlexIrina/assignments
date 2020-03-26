e1f5c362384cef4473dae646fe90719c45952cec
Alex George <alexgeorge2863@gmail.com>
2020-03-25 18:06:11 -0600

    forms are awesome

---
+++ exercises/module_3/react_forms_practice/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div>test</div>;
+  }
+}
+
+export default App;
