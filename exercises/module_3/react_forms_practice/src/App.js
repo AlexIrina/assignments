0075be442f63a93a268f1a32f1e7c51a09d2bb72
Alex George <alexgeorge2863@gmail.com>
2020-03-26 18:23:31 -0600

    forms 101 part 1

--- exercises/module_3/react_forms_practice/src/App.js
+++ exercises/module_3/react_forms_practice/src/App.js
@@ -3,10 +3,48 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      firstName: '',
+      lastName: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  // handleChange(event) {
+  //   event.preventDefault();
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   });
+  // }
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
-    return <div>test</div>;
+    return (
+      <form>
+        <input
+          type='text'
+          value={this.state.firstName}
+          name='firstName'
+          placeholder='first name'
+          onChange={this.handleChange}
+        />
+        <input
+          type='text'
+          value={this.state.lastName}
+          name='lastName'
+          placeholder='last name'
+          onChange={this.handleChange}
+        />
+        <h1>
+          {this.state.firstName} {this.state.lastName}
+        </h1>
+      </form>
+    );
   }
 }
 
