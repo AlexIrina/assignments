1d13d5bb7458bb434c852a4bea4267f605010f9f
Alex George <alexgeorge2863@gmail.com>
2020-04-02 16:36:40 -0600

    updated form

--- exercises/module_3/name_entry/src/App.js
+++ exercises/module_3/name_entry/src/App.js
@@ -9,17 +9,34 @@ class App extends Component {
       lastName: ''
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange = event => {
-    this.setState({});
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // working here
+  // When the user clicks the button, the value of the input box should be added to a running list of names that have been previous entered. (Hint: you'll need to set state and map over an array to do this).
+  handleSubmit = event => {
+    const { type, value } = event.target;
+    this.setState({
+      [type]: value
+    });
   };
 
   render() {
     return (
       <main className='App'>
-        <form>
-          <h1>List of Names</h1>
+        <form onSubmit={this.handleSubmit}>
+          <h1>
+            {this.state.firstName}
+            {this.state.lastName}
+          </h1>
+
           <input
             type='text'
             placeholder='First Name'
@@ -35,19 +52,9 @@ class App extends Component {
             value={this.state.lastName}
             onChange={this.handleChange}
           />
-          <ol className='usersNames'>
-            <li></li>
-            <br />
-            <li></li>
-            <br />
-            <li></li>
-            <br />
-            <li></li>
-            <br />
-            <li></li>
-            <br />
-            <li></li>
-          </ol>
+          <br />
+          <h2>Entered Information:</h2>
+          <ol className='usersNames'></ol>
           <button className='submitBtn'>Submit</button>
         </form>
       </main>
