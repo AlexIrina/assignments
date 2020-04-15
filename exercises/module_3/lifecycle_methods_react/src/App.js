ba15fb06cde67b0b331b01daaa5a6d213a397804
Alex George <alexgeorge2863@gmail.com>
2020-04-15 11:19:02 -0600

    cleaan slay

---
+++ exercises/module_3/lifecycle_methods_react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div>Code goes here</div>;
+  }
+}
+
+export default App;
