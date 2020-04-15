1379139d7c1ee91519331628981086c3b2199217
Alex George <alexgeorge2863@gmail.com>
2020-04-15 17:08:06 -0600

    css grid and flexbox notes

---
+++ exercises/module_3/css_grid_flexbox_refresher/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// import './App.css';
+import './grid.css';
+function App() {
+  return (
+    <div className='parent'>
+      <div className='box first'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box last'></div>
+    </div>
+  );
+}
+
+export default App;
