d0f7b055b3be48daef845a5905f3dbf6a4b8bd22
Alex George <alexgeorge2863@gmail.com>
2020-04-16 16:56:27 -0600

    played around with grid

--- exercises/module_3/css_grid_flexbox_refresher/src/App.js
+++ exercises/module_3/css_grid_flexbox_refresher/src/App.js
@@ -5,7 +5,14 @@ import './grid.css';
 function App() {
   return (
     <div className='parent'>
-      <div className='box first'></div>
+      <div className='box first'>
+        <h1 className='text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+          dicta?
+        </h1>
+      </div>
+      {/* uncomment bellow to text multiple boxes on the page */}
+      {/* <div className='box'></div>
       <div className='box'></div>
       <div className='box'></div>
       <div className='box'></div>
@@ -13,8 +20,7 @@ function App() {
       <div className='box'></div>
       <div className='box'></div>
       <div className='box'></div>
-      <div className='box'></div>
-      <div className='box last'></div>
+      <div className='box last'></div> */}
     </div>
   );
 }
