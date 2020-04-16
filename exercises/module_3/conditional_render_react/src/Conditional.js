cc9c735b50ba44bbd19dcd0f930282d4d3f8c916
Alex George <alexgeorge2863@gmail.com>
2020-04-16 09:36:42 -0600

    faked an api call

---
+++ exercises/module_3/conditional_render_react/src/Conditional.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Conditional Rendering is loading something on the screen IF the condition is true
+function Conditional(props) {
+  return (
+    <div>
+      <h1>Heres the information that you waited 1.5 seconds for</h1>
+    </div>
+  );
+}
+
+export default Conditional;
