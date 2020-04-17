1a2086f6bbaae78089b7bf9b5f35575ee46453af
Alex George <alexgeorge2863@gmail.com>
2020-04-17 06:42:47 -0600

    clean up

--- exercises/module_3/todolist_react/src/todosData.js
+++ exercises/module_3/todolist_react/src/todosData.js
@@ -1,29 +1,27 @@
-const todosData = [
+export default [
   {
     id: 1,
     text: 'Take out the trash',
-    completed: true
+    completed: true,
   },
   {
     id: 2,
     text: 'Grocery shopping',
-    completed: false
+    completed: false,
   },
   {
     id: 3,
     text: 'Clean gecko tank',
-    completed: false
+    completed: false,
   },
   {
     id: 4,
     text: 'Mow lawn',
-    completed: true
+    completed: true,
   },
   {
     id: 5,
     text: 'Catch up on Arrested Development',
-    completed: false
-  }
+    completed: false,
+  },
 ];
-
-export default todosData;
