d9aa5b2a8fb39701f5fbe1219706c7fa5d17e046
Alex George <alexgeorge2863@gmail.com>
2020-04-21 15:39:36 -0600

    start

---
+++ exercises/module_3/capstone_meme_generator/src/components/MemeGenerator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>MEME GENERATOR</div>;
+  }
+}
+
+export default MemeGenerator;
