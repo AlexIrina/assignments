2a6af13e019437d062c24c688e23686dd4222df0
Alex George <alexgeorge2863@gmail.com>
2020-05-07 09:49:47 -0600

    added context. changes from light to dark

---
+++ exercises/module_4/context/src/themeContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const ThemeContext = React.createContext();
+// // will get back a ThemeContext.Provider (to wrap around App) and ThemeContext.Consumer (lets me pass props to any component thats needs them)
+export default ThemeContext;
