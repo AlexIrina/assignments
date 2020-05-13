d2b1bd3349b9c20c57bdd71fc330e5cd6c998a1f
Alex George <alexgeorge2863@gmail.com>
2020-05-13 11:43:30 -0600

    added more fa icons

--- exercises/whoop_app/src/App.js
+++ exercises/whoop_app/src/App.js
@@ -7,40 +7,56 @@ function App() {
     <div className='container'>
       <div className='bg1'>
         <h2>
+          <i class='far fa-list-alt'></i>
+          <br />
           16 <span>| 24</span>
         </h2>
         <p>Goals Completed</p>
       </div>
       <div className='bg1'>
         <h2>
-          <i className='fas fa-battery-three-quarters'></i>
+          <i class='far fa-heart'></i>
+          <br />
+          60 <span>bpm</span>
         </h2>
-        <p>Respiration</p>
+        <p>Resting HR</p>
       </div>
       <div className='bg2'>
         <h2>
           <i className='fas fa-running'></i>
+          <br />
+          15 <span>miles</span>
         </h2>
-        <p>Miles</p>
-      </div>
-      <div className='bg1'>
-        <h2>36 &deg;</h2>
-        <p>Temperature</p>
+        <p>Distance</p>
       </div>
       <div className='bg1'>
         <h2>
-          <i className='fas fa-bed'></i>
+          <i class='fas fa-fire'></i>
+          <br />
+          200 <span>kcal</span>
         </h2>
-        <p>Sleep Keep</p>
+        <p>Calories Burned</p>
+      </div>
+      <div className='bg1'>
+        <h2>
+          <i class='fas fa-moon'></i>
+          <br />9 hrs
+        </h2>
+        <p>Sleep Quality</p>
       </div>
       <div className='bg2'>
         <h2>
-          98 <span>bpm</span>
+          <i className='fas fa-heart'></i>{' '}
+          <i class='fas fa-long-arrow-alt-up'></i>
+          <br />
+          140 <span>bpm</span>
         </h2>
-        <p>Heart Rate</p>
+        <p>Max HR</p>
       </div>
       <div className='bg1'>
         <h2>
+          <i class='fas fa-weight'></i>
+          <br />
           170 <span>lbs</span>
         </h2>
         <p>Weight</p>
@@ -53,12 +69,15 @@ function App() {
       </div>
       <div className='bg2'>
         <h2>
-          118 <span>mgdl</span>
+          <i class='fas fa-stopwatch'></i>
+          <br />
+          56: <span>30</span>
         </h2>
-        <p>Blood Glucose</p>
+        <p>Duration</p>
       </div>
       <div className='bg2'>
         <h2>
+          <i class='fas fa-drumstick-bite'></i> <br />
           680 <span>kcal</span>
         </h2>
         <p>AVG Consumption</p>
@@ -67,10 +86,12 @@ function App() {
         <h2>
           <i className='fas fa-dumbbell'></i>
         </h2>
-        <p>Workouts</p>
+        <p>My Workouts</p>
       </div>
       <div className='bg2'>
         <h2>
+          <i class='fas fa-hand-holding-water'></i>
+          <br />
           85 <span>%</span>
         </h2>
         <p>Body Hydration</p>
