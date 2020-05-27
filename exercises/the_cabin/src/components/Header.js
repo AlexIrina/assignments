6bac5ad2be6940ccd593c06abb33ddf82a5a5989
Alex George <alexgeorge2863@gmail.com>
2020-05-26 19:00:31 -0600

    made nav and header into styled components

--- exercises/the_cabin/src/components/Header.js
+++ exercises/the_cabin/src/components/Header.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from '../img/TB.svg';
+import styled from 'styled-components';
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <header>
+    <Container>
       <img className='logo' src={logo} alt='Companies Logo' />
-      <nav className='nav'>
-        <a href='/' className='nav__link'>
+      <Nav className='nav'>
+        <a href='/' className='navLink'>
           Contact
         </a>
-        <a href='/' className='nav__link'>
+        <a href='/' className='navLink'>
           About
         </a>
-        <a href='/' className='nav__link'>
+        <a href='/' className='navLink'>
           Add guests
         </a>
-        <a href='/' className='nav__link'>
+        <a href='/' className='navLink'>
           Add dates
         </a>
-      </nav>
-    </header>
+      </Nav>
+    </Container>
   );
 };
+const Container = styled.header`
+  display: flex;
+  align-items: center;
+  margin: 2em 0;
+  @media (min-width: 800px) {
+    grid-column: 2/5;
+    display: grid;
+    grid-template-columns: subgrid;
+  }
+`;
+const Nav = styled.nav`
+  /* outline: 2px solid red; */
+  /* enables the links to go to the end of the screen */
+  flex-grow: 1;
+  display: flex;
+  justify-content: space-around;
+  @media (min-width: 800px) {
+    grid-column: 2/5;
+  }
+  @media (min-width: 1200px) {
+    grid-column: 2/3;
+  }
+`;
+
+// const NavLink = styled.navLink``;
 
 export default Header;
