4348b3aefd959b6d493c0801ca78e844b5472b32
Alex George <alexgeorge2863@gmail.com>
2020-06-15 18:43:21 -0600

    converted to functional component with hooks

--- exercises/module_4/use_context_hook/src/themeContext.js
+++ exercises/module_4/use_context_hook/src/themeContext.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 const ThemeContext = React.createContext();
 
-class ThemeContextProvider extends Component {
-  state = {
-    theme: 'dark',
+function ThemeContextProvider(props) {
+  const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
 
-  toggleTheme = () => {
-    this.setState((prevState) => {
-      return {
-        theme: prevState.theme === 'light' ? 'dark' : 'light',
-      };
-    });
-  };
-
-  render() {
-    return (
-      <ThemeContext.Provider
-        value={{ theme: this.state.theme, toggleTheme: this.toggleTheme }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
 
 export { ThemeContextProvider, ThemeContext };
+
+/**
+ * Convert this ThemeContextProvider into a functional component that uses hooks
+ * Tip: Use the browser dev tools if you run into a bug
+ */
