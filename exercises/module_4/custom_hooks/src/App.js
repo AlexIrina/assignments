24d7f6f8c7dc33880cdbcec8e8c880491906f0d1
Alex George <alexgeorge2863@gmail.com>
2020-06-16 14:47:03 -0600

    pop quiz

--- exercises/module_4/custom_hooks/src/App.js
+++ exercises/module_4/custom_hooks/src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+/**
+ * Pop Quiz!
+ *
+ * Refactor the class component below to use hooks instead
+ */
 
-const App = () => {
-  return <div className='container'>Happy Coding Alex</div>;
-};
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The count is {this.state.count}</h1>
+        <button onClick={this.increment}>Add 1</button>
+      </div>
+    );
+  }
+}
 
 export default App;
