1364e0e910c3b89a746da324242a0db5709263d1
Alex George <alexgeorge2863@gmail.com>
2020-06-17 13:48:06 -0600

    refactored code. Using children instead of route

--- exercises/module_4/react_router/browser_router/src/App.js
+++ exercises/module_4/react_router/browser_router/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
     <div className='App'>
@@ -9,8 +11,12 @@ function App() {
       {/* Switches to which route to render.      Similar to a switch statement in js*/}
       <Switch>
         {/* routes me to the page */}
-        <Route exact path='/' render={() => <h1>Home Page</h1>} />
-        <Route path='/about' render={() => <h1>About Page </h1>} />
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
       </Switch>
     </div>
   );
