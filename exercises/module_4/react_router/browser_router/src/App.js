19999af714b50607722a69a9628ac0bf9916e98e
Alex George <alexgeorge2863@gmail.com>
2020-06-17 13:24:52 -0600

    added Switch and Route to my 2 pages

--- exercises/module_4/react_router/browser_router/src/App.js
+++ exercises/module_4/react_router/browser_router/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
+      {/* my links */}
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
+      {/* Switches to which route to render.      Similar to a switch statement in js*/}
+      <Switch>
+        {/* routes me to the page */}
+        <Route exact path='/' render={() => <h1>Home Page</h1>} />
+        <Route path='/about' render={() => <h1>About Page </h1>} />
+      </Switch>
     </div>
   );
 }
