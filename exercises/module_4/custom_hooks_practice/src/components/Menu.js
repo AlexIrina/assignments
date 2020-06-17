85fccd6e938205ad156e8b6cc2ec63ede299f794
Alex George <alexgeorge2863@gmail.com>
2020-06-17 10:04:48 -0600

    refactored Menu component to use my useToggler hook. Got rid of toggler component

--- exercises/module_4/custom_hooks_practice/src/components/Menu.js
+++ exercises/module_4/custom_hooks_practice/src/components/Menu.js
@@ -1,33 +1,28 @@
 import React from 'react';
-import Toggler from './Toggler';
-
-function Menu(props) {
+import useToggler from '../hooks/useToggler';
+const Menu = (props) => {
+  const [on, toggle] = useToggler();
   return (
-    <Toggler
-      defaultOnValue={true}
-      render={({ on, toggle }) => (
-        <div>
-          <button onClick={toggle}>{on ? 'Hide' : 'Show'} Menu </button>
-          <nav style={{ display: on ? 'block' : 'none' }}>
-            <h6>Signed in as Coder123</h6>
-            <p>
-              <a href='/'>Your Profile</a>
-            </p>
-            <p>
-              <a href='/'>Your Repositories</a>
-            </p>
-            <p>
-              <a href='/'>Your Stars</a>
-            </p>
-            <p>
-              <a href='/'>Your Gists</a>
-            </p>
-          </nav>
-        </div>
-      )}
-    />
+    <div>
+      <button onClick={toggle}>{on ? 'Hide' : 'Show'} Menu </button>
+      <nav style={{ display: on ? 'block' : 'none' }}>
+        <h6>Signed in as Coder123</h6>
+        <p>
+          <a href='/'>Your Profile</a>
+        </p>
+        <p>
+          <a href='/'>Your Repositories</a>
+        </p>
+        <p>
+          <a href='/'>Your Stars</a>
+        </p>
+        <p>
+          <a href='/'>Your Gists</a>
+        </p>
+      </nav>
+    </div>
   );
-}
+};
 
 export default Menu;
 // refactor component to use my custom toggler hook
