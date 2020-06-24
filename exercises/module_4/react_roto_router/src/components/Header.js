eb2eefbe13546eb16b9f2e18b98df9bd803380fd
Alex George <alexgeorge2863@gmail.com>
2020-06-23 18:26:43 -0600

    deleted logo

--- exercises/module_4/react_roto_router/src/components/Header.js
+++ exercises/module_4/react_roto_router/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Nav from './Nav';
-import logo from '../images/toolbox.png';
+// import logo from '../images/toolbox.png';
 
 const Header = () => {
   return (
     <div className='top-header'>
       <header>
-        <img className='logo' alt='' src={logo} />
+        {/* <img className='logo' alt='' src={logo} /> */}
         <Nav />
       </header>
     </div>
