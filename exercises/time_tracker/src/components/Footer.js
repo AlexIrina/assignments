1b2d3cde203f112c341116a705d0b5e6fa58a799
Alex George <alexgeorge2863@gmail.com>
2020-06-30 17:42:46 -0600

    cleaned up the list in the footer

--- exercises/time_tracker/src/components/Footer.js
+++ exercises/time_tracker/src/components/Footer.js
@@ -1,23 +1,63 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
-    <div className='user'>
+    <footer>
       <div className='user-history'>
-        <button>7/01/2020</button>
-        <button>7/02/2020</button>
-        <button>7/03/2020</button>
-        <button>7/04/2020</button>
-        <button>7/05/2020</button>
-        <button>7/06/2020</button>
-        <button>7/07/2020</button>
-        <button>7/08/2020</button>
-        <button>7/09/2020</button>
-        <button>7/10/2020</button>
-        <button>7/11/2020</button>
-        <button>7/12/2020</button>
+        <ul className='list'>
+          <li>
+            <Link>
+              <a href='/'>7/01/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/02/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/03/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/04/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/05/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/06/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/07/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/08/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/09/2020</a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a href='/'>7/10/2020</a>
+            </Link>
+          </li>
+        </ul>
       </div>
-    </div>
+    </footer>
   );
 };
 
