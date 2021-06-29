8f8b30e410ed254185fed298c06169c18e458416
Alex George <alexgeorge2863@gmail.com>
2021-06-29 16:33:54 -0600

    create a get request that sends back an array of users

--- exercises/module_5/first_express_server/server.js
+++ exercises/module_5/first_express_server/server.js
@@ -2,12 +2,19 @@
 const express = require('express')
 const app = express()
 
-app.get('/user', (req, res) => {
-	res.send({ name: 'Frodo', age: 50 })
+// TODO: send back an array of users
+const users = [
+	{ name: 'Frodo', age: 50 },
+	{ name: 'Bilbo', age: 52 },
+	{ name: 'Gandalf', age: 500 },
+]
+
+app.get('/users', (req, res) => {
+	res.send(users)
 })
 // 1: PORT     2. Callback function
 app.listen(9000, () => {
-	console.log('The server is running on local Port 9000/user')
+	console.log('The server is running on local Port 9000')
 })
 //? Route 101
 // 1. Endpoint (mount path)
@@ -15,3 +22,4 @@ app.listen(9000, () => {
 //! Request object is any CRUD request coming from the client
 //! Response object is the servers object. Response is what the server will configure and send back to the client
 // ? telling the server to listen on Port 9000/user
+// todo: Practice in Postman
